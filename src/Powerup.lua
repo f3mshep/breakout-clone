c086280cded1d1ef76d1ee876284dbf14a0cf40c
Alexandra Wright <superbiscuit@gmail.com>
2019-05-20 20:57:30 -0700

    windows git makes me so sad

---
+++ src/Powerup.lua
@@ -0,0 +1,41 @@
+Powerup = Class{}
+
+POWERUP_DIMENSION = 16
+
+function Powerup:init(spawnX, spawnY, skin, altSkin)
+    -- should be spawned at X,Y coordinates where brick was broken
+    self.x = spawnX
+    self.y = spawnY
+    self.activeSkin = skin
+    self.skin = skin
+    self.altSkin = altSkin or skin
+    self.toggled = false
+    -- start us off with no velocity
+    self.dy = 0
+
+    -- starting dimensions
+    self.width = POWERUP_DIMENSION
+    self.height = POWERUP_DIMENSION
+end
+
+function Powerup:update(count)
+  self.dy = self.dy + .1
+  self.y = self.y + self.dy
+  if count % 3 == 0 then
+    self:toggleSkin()
+  end
+end
+
+function Powerup:render()
+  love.graphics.draw(gTextures['main'], gFrames['powerups'][self.activeSkin], self.x, self.y)
+end
+
+function Powerup:toggleSkin()
+  if self.toggled then
+    self.toggled = false
+    self.activeSkin = self.skin
+  else
+    self.toggled = true
+    self.activeSkin = self.altSkin
+  end
+end
